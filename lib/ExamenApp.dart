ed4c4d7068d3f4457dc44bd8a0d17b30ade315a6
Marcos García <markarules01@gmail.com>
2023-12-09 12:49:55 +0100

    PMDM: P1) Puesta en marcha de la aplicacion con la declaracion de clases y el runApp (ExamenApp).

---
+++ lib/ExamenApp.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'OnBoarding/LoginView.dart';
+import 'OnBoarding/RegisterView.dart';
+import 'OnBoarding/SplashView.dart';
+
+class ExamenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp materialApp;
+    if (kIsWeb) {
+      materialApp = MaterialApp(
+        title: "Examen App",
+        routes: {
+          '/loginview': (context) => LoginView(),
+          '/registerview': (context) => RegisterView(),
+          //'/homeview':(context) => HomeView(),
+          '/splashview': (context) => SplashView(),
+        },
+        initialRoute: '/splashview',
+      );
+    } else
+      materialApp = MaterialApp(
+        title: "Examen App",
+        routes: {
+          '/loginview': (context) => LoginView(),
+          '/registerview': (context) => RegisterView(),
+          //'/homeview':(context) => HomeView2(),
+          '/splashview': (context) => SplashView(),
+        },
+        initialRoute: '/splashview',
+      );
+    return materialApp;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'ExamenApp.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
+  runApp(ExamenApp());
 }
