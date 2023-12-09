c467ff986491fbf47ad937eeabf046b7d590f2a0
Marcos García <markarules01@gmail.com>
2023-12-09 14:02:21 +0100

    PMDM: P2 & P3) (P2)Adición de las funciones necesarias en los botones correspondientes además de otras enviadas por parámetro. (P3)Implementadas llamadas a Firebase usando FirebaseAdmin para cargar posts necesarios.

--- lib/SingleTone/DataHolder.dart
+++ lib/SingleTone/DataHolder.dart
@@ -1,14 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../FirestoreObjects/FbPosts.dart';
 import 'FirebaseAdmin.dart';
 
 class DataHolder {
   static final DataHolder _dataHolder = new DataHolder._internal();
   FirebaseFirestore db = FirebaseFirestore.instance;
   FirebaseAdmin fbAdmin = FirebaseAdmin();
+  String sNombre = "DataHolder Examen";
+  late String sPostTitle;
+  FbPosts? selectedPost;
 
   factory DataHolder() {
     return _dataHolder;
   }
 
-  DataHolder._internal();
+  void initDataHolder() {
+    sPostTitle = "Titulo de Post";
+  }
+
+  Future<FbPosts?> initCachedFbPost() async {
+    if (selectedPost != null) return selectedPost;
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? fbpost_titulo = prefs.getString('titulo');
+    fbpost_titulo ??= "";
+
+    String? fbpost_cuerpo = prefs.getString('cuerpo');
+    fbpost_cuerpo ??= "";
+
+    String? fbpost_imagen = prefs.getString('imagen');
+    fbpost_imagen ??= "";
+
+    selectedPost = FbPosts(
+        titulo: fbpost_titulo, cuerpo: fbpost_cuerpo, imagen: fbpost_imagen);
+
+    return selectedPost;
+  }
+
+  DataHolder._internal() {
+    initCachedFbPost();
+  }
 }
--- lib/SingleTone/FirebaseAdmin.dart
+++ lib/SingleTone/FirebaseAdmin.dart
@@ -1,7 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../FirestoreObjects/FbPosts.dart';
 
 class FirebaseAdmin {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore db = FirebaseFirestore.instance;
+  FbPosts? selectedPost;
+
+  void crearPostEnFB(FbPosts post) {
+    CollectionReference<FbPosts> postRef = db.collection("Post").withConverter(
+        fromFirestore: FbPosts.fromFirestore,
+        toFirestore: (FbPosts post, _) => post.toFirestore());
+
+    postRef.add(post);
+  }
+
+  void saveSelectedPostInCache() async {
+    if (selectedPost != null) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('titulo', selectedPost!.titulo);
+      prefs.setString('cuerpo', selectedPost!.cuerpo);
+      prefs.setString('imagen', selectedPost!.imagen);
+    }
+  }
 }
